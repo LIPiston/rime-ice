a7461f70da67cb34c0bc94df5dbd3a9fcc722e23
LIPiston <wps1910093789@gmail.com>
2025-03-20 17:13:49 +0800

    fix: 修复语言模型的下载脚本的缩进
    fix: 修复语言模型的ps1脚本

--- others/get-grammar-bash.sh
+++ others/get-grammar-bash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-     curl -LO https://github.com/boomker/rime-fast-xhup/releases/download/v1.0.0/zh-hans-t-huayu-v7-bgw.gram
-     tee rime_ice.schema.custom.yaml >/dev/null <<EOF
-     patch:
-       "grammar/language": zh-hans-t-huayu-v7-bgw
-       "grammar/collocation_max_length": 5
-       "grammar/collocation_min_length": 2
-       "translator/contextual_suggestions": true
-       "translator/max_homophones": 7
-       "translator/max_homographs": 7
+    curl -LO https://github.com/boomker/rime-fast-xhup/releases/download/v1.0.0/zh-hans-t-huayu-v7-bgw.gram
+    tee rime_ice.schema.custom.yaml >/dev/null <<EOF
+    patch:
+      "grammar/language": zh-hans-t-huayu-v7-bgw
+      "grammar/collocation_max_length": 5
+      "grammar/collocation_min_length": 2
+      "translator/contextual_suggestions": true
+      "translator/max_homophones": 7
+      "translator/max_homographs": 7
 EOF
